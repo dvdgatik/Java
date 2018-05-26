747aafd8591ae1baec4d97e557522a520601c06d
david <david@david-Aspire-V3-574>
2018-05-26 00:34:54 -0500

    1. Clases en llava

---
+++ src/cursojava/Persona.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursojava;
+import java.util.Date;
+
+/**
+ *
+ * @author david
+ */
+public class Persona {
+    protected int id;
+    String Nombre;
+    public String Apellidos;
+    private Date fechaNac;
+}
